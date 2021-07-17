157cd9d66191ea436697893d7a656ab1bbe34954
Ahmed Samy <serenitydiver@hotmail.com>
2021-07-17 16:28:30 +0300

    Adding Routing Project to learn how to navigate in angular

---
+++ Routing/simpleRouting/src/app/app-routing.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DepartmentContactComponent } from './department-contact/department-contact.component';
+import { DepartmentDetailComponent } from './department-detail/department-detail.component';
+import { DepartmentOverviewComponent } from './department-overview/department-overview.component';
+
+import { DepartmentComponent } from './department/department.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/departments', pathMatch: 'full' },
+  { path: 'departments', component: DepartmentComponent },
+  {
+    path: 'departments/:id',
+    component: DepartmentDetailComponent,
+    children: [
+      { path: 'overview', component: DepartmentOverviewComponent },
+      { path: 'contact', component: DepartmentContactComponent },
+    ],
+  },
+  { path: 'employee', component: EmployeeComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
+export const routingComponents = [
+  DepartmentComponent,
+  EmployeeComponent,
+  PageNotFoundComponent,
+  DepartmentDetailComponent,
+  DepartmentOverviewComponent,
+  DepartmentContactComponent,
+];
---
+++ Routing/simpleRouting/src/app/department/department.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+
+@Component({
+  selector: 'app-department',
+  templateUrl: './department.component.html',
+  styleUrls: ['./department.component.css'],
+})
+export class DepartmentComponent implements OnInit {
+  departments = [
+    { id: 1, name: 'Angular' },
+    { id: 2, name: 'Node' },
+    { id: 3, name: 'MongoDB' },
+    { id: 4, name: 'Java' },
+    { id: 5, name: 'JavaScript' },
+  ];
+
+  public selectedId: number;
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      let id = parseInt(params.get('id'));
+      this.selectedId = id;
+    });
+  }
+
+  onSelect(department: any) {
+    //this.router.navigate(['/departments', department.id]);
+    this.router.navigate([department.id], { relativeTo: this.route });
+  }
+
+  isSelected(department: any) {
+    department.id === this.selectedId;
+  }
+}
