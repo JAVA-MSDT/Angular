598afe3bf227a76c9c25e49e4a31c8caa9337c62
Ahmed Samy <serenitydiver@hotmail.com>
2023-06-09 16:33:11 +0200

    Adding products and initalizing the project

---
+++ products/src/app/products/product-page/product-page.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Product } from 'src/app/domains/Product';
+
+@Component({
+  selector: 'app-product-page',
+  templateUrl: './product-page.component.html',
+  styleUrls: ['./product-page.component.css']
+})
+export class ProductPageComponent {
+
+  product: Product = {
+    title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    price: 109.95,
+    description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    category: "men's clothing",
+    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+  }
+}
